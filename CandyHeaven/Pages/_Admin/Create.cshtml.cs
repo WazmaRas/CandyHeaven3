a9e4f3ea30035acb490f5c13e296339d896d705f
WazmaRas <wazma.rasouli@live.se>
2021-11-15 13:44:00 +0100

    create ändringar

--- CandyHeaven/Pages/_Admin/Create.cshtml.cs
+++ CandyHeaven/Pages/_Admin/Create.cshtml.cs
@@ -8,38 +8,46 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CandyHeaven.Pages._Admin
 {
-    public class IndexModel : PageModel
+    public class CreateModel : PageModel
     {
         public List<Models.Product> Products = Data.ProductManager.Products;
         [BindProperty]
         [Required]
-        public int Id { get; set; }      
+        public int Id { get; set; }
         [BindProperty]
         [Required]
-        public string Name { get; set; }     
+        public string Name { get; set; }
         [BindProperty]
         [Required]
-        public string Description { get; set; }    
+        public string Description { get; set; }
         [BindProperty]
         [Required]
-        public double Price { get; set; }    
+        public double Price { get; set; }
         [BindProperty]
         [Required]
-        public bool Featured { get; set; }     
+        public bool Featured { get; set; }
         [BindProperty]
         [Required]
         public int Stock { get; set; }
         [BindProperty]
         [Required]
-        public string ImageURL { get; set; }   
+        public string ImageURL { get; set; }
+        [BindProperty]
+        [Required]
+        public string Taste { get; set; }
+        [BindProperty]
+        [Required]
+        public int Weight { get; set; }
         [BindProperty]
         [Required]
         public string CandySort { get; set; }
-        
+        [BindProperty]
+        public string Catogory { get; set; }
+
 
         public void OnPost(int id)
         {
-            Models.Product newProduct = new Models.BulkCandy(Id, Name, Description, Featured, Price, ImageURL, Stock, CandySort );
+            Models.Product newProduct = new Models.BulkCandy(Id, Name, Description, Featured, Price, ImageURL, Stock, CandySort);
 
             Data.ProductManager.AddProduct(newProduct);
 
@@ -49,8 +57,8 @@ namespace CandyHeaven.Pages._Admin
 
 
 
-        }
-         
+
+
         }
     }
-
+}
---
+++ CandyHeaven/Pages/_Admin/Details.cshtml.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CandyHeaven.Pages._Admin
+{
+    public class DetailsModel : PageModel
+    {
+
+        public List<Models.Product> Products = Data.ProductManager.Products;
+        public void OnGet()
+        {
+        }
+    }
+}
